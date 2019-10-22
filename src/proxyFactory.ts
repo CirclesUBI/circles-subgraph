025a1f2715135a08a7bf93916bec1247fd1aba63
anon <sarah.anne.friend@gmail.com>
2019-10-21 23:07:01 -0400

    Store user from call, begin add user support

--- src/proxyFactory.ts
+++ src/proxyFactory.ts
@@ -1,9 +1,11 @@
 import {
   ProxyCreation as ProxyCreationEvent,
+  CreateProxyCall,
 } from './types/ProxyFactory/ProxyFactory'
 
 import {
   Safe,
+  User,
 } from './types/schema'
 
 import { Bytes, BigInt, Address } from "@graphprotocol/graph-ts";
@@ -11,4 +13,11 @@ import { Bytes, BigInt, Address } from "@graphprotocol/graph-ts";
 export function handleProxyCreation(event: ProxyCreationEvent): void {
   let safe = new Safe(event.params.proxy.toHex())
   safe.save()
+}
+
+export function handleCreateProxyCall(call: CreateProxyCall): void {
+  let id = call.transaction.hash.toHex()
+  let user = new User(call.from.toHex())
+  user.safe = call.outputs.proxy.toHex()
+  user.save()
 }
